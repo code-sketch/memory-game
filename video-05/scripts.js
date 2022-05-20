10617be5c6affcc11fe026f2c543b68320082cb8
Rob <fadedrob@users.noreply.github.com>
2022-05-19 23:14:36 -0700

    Remove video-## folders

--- video-05/scripts.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll('.memory-card');
-
-function flipCard() {
-  this.classList.toggle('flip');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
